d9271d04f8e5faebf3ea1d9eef99bb7a08c304c0
Mauro <dantuchiii@gmail.com>
2022-08-28 17:10:32 -0300

    Segundo commit

--- app.js
+++ app.js
@@ -4,17 +4,9 @@ const colors = require('colors');
 
 console.clear();
 
-// console.log(argv);
-
-// console.log('base: yargs', argv.b);
-// console.log('listar', argv.l)
 const base = argv.b;
 const listar = argv.l;
 const hasta = argv.h;
-
-// const [ , , arg3 = 'base=5'] = process.argv;
-// const [,base] = arg3.split('=');
-// const base = 8;
 
 crearArchivo(base, listar, hasta)
     .then(nombreArchivo => console.log(nombreArchivo.rainbow, ' creado'))
--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,29 +1,6 @@
 const fs = require('fs');
 var colors = require('colors');
 
-
-// const crearArchivo = ( base = 5) => {
-
-//     return new Promise((resolve, reject) => {
-
-//         try {
-//             let salida = '';
-
-//             for (let index = 1; index <= 10; index++){
-//                 salida += (`${base} x ${index} = ${base * index}\n`);
-//             }
-
-//             fs.writeFileSync(`tabla-${base}.txt`, salida);
-            
-//             resolve (`tabla-${base}.txt`);
-//         } catch (error) {
-//             reject('Error');
-//         }
-        
-//     })  
-
-// }
-
 const crearArchivo = async( base = 5, listar, hasta = 10) => {    
 
         try {
@@ -35,8 +12,7 @@ const crearArchivo = async( base = 5, listar, hasta = 10) => {
 
             if (listar) {
                 console.log(salida);
-            }
-            
+            }       
 
             fs.writeFileSync(`./salida/tabla-${base}.txt`, salida);
             
